4b641f377c4ae5d78ac7c6b41a79f2a55689cdd8
Yuliim <yulim00610@naver.com>
2026-01-09 01:47:50 +0900

    style: 코파일럿 리뷰어에게 정리 안 된 실험 코드 때문에 Merge 거부를 먹었으나, cross-encoder는 추후에 사용할 수도 있는 파일이라고 생각되어 그냥 관련 코드 모두 주석처리 함

--- vectorstore/cross_encoder.py
+++ vectorstore/cross_encoder.py
@@ -1,39 +1,39 @@
-# cross_encoder.py
+# # cross_encoder.py
 
-from sentence_transformers import CrossEncoder  # Cross-Encoder 로드
+# from sentence_transformers import CrossEncoder  # Cross-Encoder 로드
 
-_reranker = None  # 전역 캐시
+# _reranker = None  # 전역 캐시
 
-def get_reranker(model_name: str):
-    global _reranker
-    if _reranker is None:
-        _reranker = CrossEncoder(model_name)
-    return _reranker
+# def get_reranker(model_name: str):
+#     global _reranker
+#     if _reranker is None:
+#         _reranker = CrossEncoder(model_name)
+#     return _reranker
 
 
-class CrossEncoderReranker:
-    def __init__(self, model_name: str):
-        # Cross-Encoder 모델 초기화
-        self.model = CrossEncoder(model_name)
+# class CrossEncoderReranker:
+#     def __init__(self, model_name: str):
+#         # Cross-Encoder 모델 초기화
+#         self.model = CrossEncoder(model_name)
 
-    def rerank(self, query: str, docs_with_scores: list, top_n: int):
-        # (query, document) 쌍 생성
-        pairs = [
-            (query, doc.page_content)  # 질의 + 문서 내용
-            for doc, _ in docs_with_scores
-        ]
+#     def rerank(self, query: str, docs_with_scores: list, top_n: int):
+#         # (query, document) 쌍 생성
+#         pairs = [
+#             (query, doc.page_content)  # 질의 + 문서 내용
+#             for doc, _ in docs_with_scores
+#         ]
 
-        # Cross-Encoder 점수 예측
-        scores = self.model.predict(pairs)
+#         # Cross-Encoder 점수 예측
+#         scores = self.model.predict(pairs)
 
-        # 문서 + 점수 결합
-        reranked = [
-            (docs_with_scores[i][0], scores[i])  # (doc, cross_score)
-            for i in range(len(scores))
-        ]
+#         # 문서 + 점수 결합
+#         reranked = [
+#             (docs_with_scores[i][0], scores[i])  # (doc, cross_score)
+#             for i in range(len(scores))
+#         ]
 
-        # 점수 내림차순 정렬
-        reranked.sort(key=lambda x: x[1], reverse=True)
+#         # 점수 내림차순 정렬
+#         reranked.sort(key=lambda x: x[1], reverse=True)
 
-        # 상위 top_n개 반환
-        return reranked[:top_n]
+#         # 상위 top_n개 반환
+#         return reranked[:top_n]
